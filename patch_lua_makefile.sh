cb39dab578422c59678c727b5c51875e433d0bf3
Mark Dietzer <mriq91@gmail.com>
2014-09-09 17:44:54 +0200

    Use sed, get rid of patch for luaconf.h

--- patch_lua_makefile.sh
+++ patch_lua_makefile.sh
@@ -1,45 +1,60 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -xe
 
 PATCH_FILE=lua/src/Makefile
 PATCH_FILE_BACKUP=$PATCH_FILE~
-
-if [ -e "$PATCH_FILE_BACKUP" ]; then
-	set -x
-        rm -f "$PATCH_FILE"
-        mv "$PATCH_FILE_BACKUP" "$PATCH_FILE"
-	set +x
-fi
 
 PATCH_FILE_H=lua/src/luaconf.h
 PATCH_FILE_H_BACKUP=$PATCH_FILE_H~
 
 if [ "x$1" = "xclean" ]; then
 	if [ -e "$PATCH_FILE_H_BACKUP" ]; then
-		set -x
-	        rm -f "$PATCH_FILE_H"
-	        mv "$PATCH_FILE_H_BACKUP" "$PATCH_FILE_H"
-		set +x
+		rm -f "$PATCH_FILE_H"
+		mv "$PATCH_FILE_H_BACKUP" "$PATCH_FILE_H"
+	fi
+
+	if [ -e "$PATCH_FILE_BACKUP" ]; then
+		rm -f "$PATCH_FILE"
+		mv "$PATCH_FILE_BACKUP" "$PATCH_FILE"
 	fi
 
 	exit 0
 fi
 
-set -x
-sed -ri~ '
-# Replace AR with ARR
-s/\$\(AR\)/$(ARR)/g; t
+if [ ! -e "$PATCH_FILE_BACKUP" ]; then
+	sed -ri~ '
+	# Replace AR with ARR
+	s/\$\(AR\)/$(ARR)/g; t
 
-# Remove some variables
-s/^(CC|LDFLAGS|RANLIB|RM)=/#\0/; t
+	# Remove some variables
+	s/^(CC|LDFLAGS|RANLIB|RM)=/#\0/; t
 
-# Replace AR assignment
-s/^AR=.*$/ARR=$(AR) rcu/; t
+	# Replace AR assignment
+	s/^AR=.*$/ARR=$(AR) rcu/; t
 
-# Remove -lm from LIBS
-s/^(LIBS=.*)-lm$/\1/; t
-' "$PATCH_FILE"
+	# Remove -lm from LIBS
+	s/^(LIBS=.*)-lm$/\1/; t
+	' "$PATCH_FILE"
+fi
 
 if [ ! -e "$PATCH_FILE_H_BACKUP" ]; then
-	cp "$PATCH_FILE_H" "$PATCH_FILE_H_BACKUP"
-	cd lua && patch -Nf -i ../emscripten_luaconf.patch -p1 && cd ..
+	mv "$PATCH_FILE_H" "$PATCH_FILE_H_BACKUP"
+	sed -r '
+		# We are in the zone => branch
+		/[[:space:]]*\*+[[:space:]]*[Ll]ocal configuration/,+10 b localconfig
+
+		# If there is emlua_conf elsewhere, something is fishy => exit with error
+		/emlua_conf/ q 1
+
+		# next line
+		b
+
+		:localconfig
+
+		# If there is something else with emlua_conf here, something is fishy => exit with error
+		/emlua_conf/ q 1
+
+		s!#endif!#include "../../emlua_conf.h"\n\n\0!
+	' "$PATCH_FILE_H_BACKUP" > "$PATCH_FILE_H"
 fi
