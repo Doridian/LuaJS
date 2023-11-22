7a17e52352bd79da137f200be73bf61bede79887
Doridian <git@doridian.net>
2023-11-22 15:13:42 -0800

    handle async JS errors

--- src/jslua_async.c
+++ src/jslua_async.c
@@ -4,14 +4,22 @@
 #include "jsvar_definitions.h"
 
 
-EM_ASYNC_JS(void, jslua_await_int, (lua_State *state, int ref), {
+EM_ASYNC_JS(int, jslua_await_int, (lua_State *state, int ref), {
   const val = Module.__getVarByRef(ref);
-  const result = await val;
-  Module.__pushVar(state, result);
+  try {
+    const result = await val;
+    Module.__pushVar(state, result);
+  } catch (e) {
+    Module.__pushVar(state, e.toString());
+    return 0;
+  }
+  return 1;
 });
 
 int luajs_await(lua_State *L) {
   GET_SelfTypedPointerData();
-  jslua_await_int(L, data->ptr);
+  if (!jslua_await_int(L, data->ptr)) {
+    lua_error(L);
+  }
   return 1;
 }
