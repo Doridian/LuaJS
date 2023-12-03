092a96f22b7d93c3eb02828b60422738c6e23a9d
Amrit Bhogal <ambhogal01@gmail.com>
2023-12-03 11:17:09 -0800

    Added fetch loader it can load files from the web server using `js.global.fetch` WIP support for fetching an arbretrary URL is in this commit

--- fsroot/lua/init.lua
+++ fsroot/lua/init.lua
@@ -1,3 +1,7 @@
+--Making these local makes the script faster
+local js = js
+local global = js.global
+
 local function __jsmt_addrecurse(tbl)
 	local tbl_toTable = tbl.toTable
 
@@ -6,28 +10,28 @@ local function __jsmt_addrecurse(tbl)
 		if not recursive then
 			return ret
 		end
-		
+
 		maxDepth = (maxDepth or 10) - 1
 		if maxDepth <= 0 then
 			return nil
 		end
-		
+
 		for k,v in next, ret do
 			if v and type(v) == 'userdata' and v.__isJavascript and v:__isJavascript() and v.toTable then
 				ret[k] = v:toTable(true, maxDepth)
 			end
 		end
-		
+
 		return ret
 	 end
 end
 __jsmt_addrecurse(js.__mt_js_object)
 __jsmt_addrecurse(js.__mt_js_array)
 
-local __js_Object_entries = js.global.Object.entries
-local __js_Symbol_iterator = js.global.Symbol.iterator
-local __js_Map_prototype = js.global.Map.prototype
-local __js_Object_get_prototype = js.global.Object.getPrototypeOf
+local __js_Object_entries = global.Object.entries
+local __js_Symbol_iterator = global.Symbol.iterator
+local __js_Map_prototype = global.Map.prototype
+local __js_Object_get_prototype = global.Object.getPrototypeOf
 local unpack = table.unpack
 
 function js.__mt_js_object:get_prototype()
@@ -84,3 +88,85 @@ function js.__mt_js_object:__pairs()
 
 	return self:entries_iterator()
 end
+
+
+---@param url string
+---@param options { [string] : string }?
+---@return boolean ok, string text
+local function fetch_text(url, options)
+	return pcall(function()
+		local req = global:fetch(url, options)
+		if not req then error("failed to fetch") end
+
+		local file = js.await(req)
+		if not file.ok then error("failed to fetch: "..file.status) end
+		return js.await(file:text())
+	end)
+end
+
+---@param url string
+---@return boolean, string? error
+local function file_exists(url)
+	local req = global:fetch(url, { method = "HEAD" })
+	if not req then return false end
+
+	local file = js.await(req)
+	if not file or not file.ok then return false, file.status end
+	return true
+end
+
+---Creates a searcher function that fetches modules from the web server
+---@param extension string
+---@param is_native boolean
+---@return fun(module: string): function | string
+local function fetch_searcher(extension, is_native)
+	return function(module)
+		-- TODO: When there is `require` in the loaded module, it will try to load it from the web server
+		-- we need to prepend the module require with the url to make it work in the case of multiple files
+		-- if module:find("^https://") then
+		-- 	local ok, err = fetch_text(module, {
+		-- 		method = "GET",
+		-- 		mode = "cors",
+		-- 	})
+
+		-- 	if ok then
+		-- 		local ok, err = load(err, "@"..module)
+		-- 		return ok or ("Could not load: "..(err or "Unknown error"))
+		-- 	else
+		-- 		return "Failed to fetch: "..err
+		-- 	end
+		-- end
+		local search_path = (is_native and package.cpath or package.path):gsub("%.so", extension):gsub("%.lua", extension)
+		local module_path = module:gsub("%.", "/")
+
+		---@type string[]
+		local checked_paths = {}
+		for path in search_path:gmatch("[^;]+") do
+			local test_path = path:gsub("%?", module_path)
+			local exists, err = file_exists(test_path)
+			if exists then
+				if is_native then
+					error("Cannot load native modules from the web yet")
+					-- return package.loadlib(test_path, "luaopen_"..module:gsub("%.", "_"))
+				else
+					local ok, text = fetch_text(test_path)
+					if ok then
+						local ok, err = load(text, "@"..test_path)
+						return ok or ("Could not load: "..(err or "Unknown error"))
+					else
+						return "Failed to fetch: "..text
+					end
+				end
+			end
+
+			table.insert(checked_paths, test_path)
+		end
+
+		return "Could not find module: " ..
+		module.." on web server (checked paths: \n\t\t - "..table.concat(checked_paths, "\n\t\t - ")
+	end
+end
+
+package.searchers[#package.searchers + 1] = fetch_searcher(".lua", false)
+package.searchers[#package.searchers + 1] = fetch_searcher(".so", true)
+package.searchers[#package.searchers + 1] = fetch_searcher(".wasm", true)
